758577ae981122679624ba555c03b715f125d074
sim <simyechan87@gmail.com>
2024-01-11 11:18:40 +0900

    페이지 삭제 구현

--- Nodejs-master/main.js
+++ Nodejs-master/main.js
@@ -33,7 +33,7 @@ app.get('/page/:pageId', function(request, response) {
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
           <a href="/update/${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
+          <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
@@ -116,10 +116,24 @@ app.post('/update_process', function(request, response) {
     var description = post.description;
     fs.rename(`data/${id}`, `data/${title}`, function(error){
       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
+        response.redirect(`/?id=${title}`);
       })
     });
+  });
+});
+
+app.post('/delete_process', function(request, response) {
+  var body = '';
+  request.on('data', function(data){
+    body = body + data;
+  });
+  request.on('end', function(){
+    var post = qs.parse(body);
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error){
+      response.redirect('/');
+    })
   });
 });
 
